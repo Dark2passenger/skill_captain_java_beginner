6e096e373d6fd60af162c58ac6adaa495107f44d
Goutham <145991297+Dark2passenger@users.noreply.github.com>
2023-09-27 19:47:10 +0530

    Create Day2.java

---
+++ Day2.java
@@ -0,0 +1,21 @@
+package skill_captain_java_beginner;
+
+public class DataTypes {
+    public static void main(String[] args){
+        char restaurantTier = 'A';
+        float restaurantCustomerRating = 4.5F;
+        float restaurantCriticRating = 4.7F;
+        short restaurantTotalBranches = 3400;
+        int restaurantTotalEmployees = 34000;
+        double restaurantYearlyTurnover = 40.3456;
+        System.out.println("YOUR FAVORITE RESTAURANT DETAILS");
+        System.out.println("---------------------------------");
+        System.out.println("Restaurant TIer: "+ restaurantTier);
+        System.out.println("Restaurant Customer Rating: "+ restaurantCustomerRating);
+        System.out.println("Restaurant Critic Rating: "+ restaurantCriticRating);
+        System.out.println("Total No of Branches: "+restaurantTotalBranches);
+        System.out.println("Total No of Employees: "+ restaurantTotalEmployees);
+        System.out.println("Yearly Turnover: "+ restaurantYearlyTurnover+"Cr");
+
+    }
+}
